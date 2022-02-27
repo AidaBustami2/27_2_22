43d7b86e7299facf599817231b07084c24b9f1f3
AidaBustami2 <47245468+AidaBustami2@users.noreply.github.com>
2022-02-27 07:47:34 -0500

    Update script6.sh

--- test2/script6.sh
+++ test2/script6.sh
@@ -7,7 +7,7 @@ then
 fi
 
 docker build /root/test2 --rm   -t img1
-docker run -d --privileged=true  -ti --name AIDA -e container=docker  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /run   -v /var/www/html:/var/www/html    -p 7070:8899 img1 
+docker run -d --privileged=true  -ti --name AIDA -e container=docker  -v /sys/fs/cgroup:/sys/fs/cgroup  -v /var/www/html:/var/www/html    -p 7070:8899 img1 
 
 
 docker exec -it AIDA /root/test/isInstalled.sh
